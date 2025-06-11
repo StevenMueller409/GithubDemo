d44df87424bf10ec3f7a65b2de3f381530c47bac
StevenMueller409 <59574797+StevenMueller409@users.noreply.github.com>
2025-06-10 19:08:26 -0600

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
-print("Hello from LASP!")
-print("Hello from LASP!")
-print("Hello from LASP!")
-print("Hello from LASP!")
-print("Hello from LASP!")
+def greet(name):
+    message = f"Hello, {name}!"
+    return message
+
+if __name__ == "__main__":
+    name = "World"
+    print(greet(name))
